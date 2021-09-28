219bf0d0b1ecfa0f56f8a07ad6e9839d2d8e2afc
Onuraslann <theeaslann1@gmail.com>
2021-09-28 23:33:06 +0300

    Datatable ajax ile silme işlemi gerçekleştirildi

---
+++ RentalCarMvcProject/Scripts/custom.js
@@ -0,0 +1,126 @@
+﻿
+$(function () {
+    $("#tblBrand").DataTable()
+    $("#tblBrand").on("click", ".btnBrandDelete", function () {
+        var btn = $(this);
+        bootbox.confirm("Departmanı silmek istediğinize eminmisiniz ? ", function (result) {
+            if (result) {
+                var id = btn.data("id");
+
+                $.ajax({
+
+                    type: "GET",
+                    url: "/Brand/Delete/" + id,
+                    success: function () {
+
+                        btn.parent().parent().remove();
+                    }
+
+
+                });
+            }
+
+        });
+    });
+
+});
+$(function () {
+    $("#tblCar").DataTable()
+    $("#tblCar").on("click", ".btnCarDelete", function () {
+        var btn = $(this);
+        bootbox.confirm("Departmanı silmek istediğinize eminmisiniz ? ", function (result) {
+            if (result) {
+                var id = btn.data("id");
+
+                $.ajax({
+
+                    type: "GET",
+                    url: "/Car/Delete/" + id,
+                    success: function () {
+
+                        btn.parent().parent().remove();
+                    }
+
+
+                });
+            }
+
+        });
+    });
+
+});
+$(function () {
+    $("#tblCarImage").DataTable()
+    $("#tblCarImage").on("click", ".btnCarImageDelete", function () {
+        var btn = $(this);
+        bootbox.confirm("Departmanı silmek istediğinize eminmisiniz ? ", function (result) {
+            if (result) {
+                var id = btn.data("id");
+
+                $.ajax({
+
+                    type: "GET",
+                    url: "/CarImage/Delete/" + id,
+                    success: function () {
+
+                        btn.parent().parent().remove();
+                    }
+
+
+                });
+            }
+
+        });
+    });
+
+});
+$(function () {
+    $("#tblColor").DataTable()
+    $("#tblColor").on("click", ".btnColorDelete", function () {
+        var btn = $(this);
+        bootbox.confirm("Departmanı silmek istediğinize eminmisiniz ? ", function (result) {
+            if (result) {
+                var id = btn.data("id");
+
+                $.ajax({
+
+                    type: "GET",
+                    url: "/Color/Delete/" + id,
+                    success: function () {
+
+                        btn.parent().parent().remove();
+                    }
+
+
+                });
+            }
+
+        });
+    });
+
+});
+$(function () {
+    $("#tblCustomer").DataTable()
+    $("#tblCustomer").on("click", ".btnCustomerDelete", function () {
+        var btn = $(this);
+        bootbox.confirm("Departmanı silmek istediğinize eminmisiniz ? ", function (result) {
+            if (result) {
+                var id = btn.data("id");
+
+                $.ajax({
+
+                    type: "GET",
+                    url: "/Customer/Delete/" + id,
+                    success: function () {
+
+                        btn.parent().parent().remove();
+                    }
+
+
+                });
+            }
+
+        });
+    });
+
+});
