5cb3f5f9f57ad1162dafca3ae081df23486e53a3
Onuraslann <theeaslann1@gmail.com>
2021-09-28 21:15:11 +0300

    Add project files.

---
+++ RentalCarMvcProject/Controllers/ColorController.cs
@@ -0,0 +1,19 @@
+﻿using RentalCarMvcProject.Models.EntityFramework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace RentalCarMvcProject.Controllers
+{
+    public class ColorController : Controller
+    {
+        RentalCarEntities db = new RentalCarEntities();
+        public ActionResult Index()
+        {
+            var model = db.Colors.ToList();
+            return View(model);
+        }
+    }
+}
---
+++ RentalCarMvcProject/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="RentalCarMvcProject.MvcApplication" Language="C#" %>
